8844420dd2dd954c32e3817a26427de1280dc722
gwenzek <gwenzek@users.noreply.github.com>
2017-10-17 13:44:06 +0200

    formatting

--- set_window_title.py
+++ set_window_title.py
@@ -38,7 +38,7 @@ class SetWindowTitle(EventListener):
 
   def run(self, view):
     if not self.script_path:
-      print("SetWindowTitle: ST haven't finished loading yet, skipping.")
+      print("[SetWindowTitle] Info: ST haven't finished loading yet, skipping.")
       return
 
     view_name = view.name() or view.file_name()
@@ -50,10 +50,15 @@ class SetWindowTitle(EventListener):
     view.settings().set(WAS_DIRTY, view.is_dirty())
 
   def get_project(self, view):
-    project = view.window().project_file_name()
+    project = None
+    window = view.window()
+    if not window:
+      return
+
+    project = window.project_file_name()
     if not project:
-      folders = view.window().folders()
-      project = folders[0] if folders else ''
+      folders = window.folders()
+      project = folders[0] if folders else ""
     if project:
       project = os.path.basename(project)
       project = os.path.splitext(project)[0]
@@ -61,10 +66,9 @@ class SetWindowTitle(EventListener):
     return project
 
   def get_official_title(self, view, view_name, project):
-    """
-    Returns the official name for a given view.
+    """Returns the official name for a given view.
 
-    /!\ The full file path isn't computed,
+    Note: The full file path isn't computed,
     because ST uses `~` to shorten the path.
     """
     official_title = os.path.basename(view_name) if view_name else "untitled"
@@ -75,10 +79,8 @@ class SetWindowTitle(EventListener):
     official_title += " - Sublime Text"
     return official_title
 
-  def get_new_title(self, view, view_name, project):
-    """
-    Returns the new name for a given view, according to the user preferences.
-    """
+  def get_new_title(self, view, view_name, project, old_title):
+    """Returns the new name for a view, according to the user preferences."""
     settings = sublime.load_settings("set_window_title.sublime-settings")
 
     full_path = view_name or settings.get('untitled')
@@ -86,16 +88,18 @@ class SetWindowTitle(EventListener):
 
     # Don't try to compute relative path if we don't have a file path.
     if view.file_name():
-      folders = view.window().folders()
+      full_path = view.file_name()
+      window = view.window()
+      folders = window.folders() if window else None
       root = folders[0] if folders else None
       if root:
         rel_path = os.path.relpath(view.file_name(), root)
 
-    template = settings.get('template')
+    template = settings.get("template")
     template = self._replace_condition(
-        template, 'has_project', project, settings)
+        template, "has_project", project, settings)
     template = self._replace_condition(
-        template, 'is_dirty', view.is_dirty(), settings)
+        template, "is_dirty", view.is_dirty(), settings)
 
     return template.format(
         rel_path=rel_path, full_path=full_path, project=project)
