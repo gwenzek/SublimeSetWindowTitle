5c365174d27ffb71cf758e2975d99ba7ab5b66b9
robertcollier4 <robertcollier444@gmail.com>
2019-09-04 20:45:48 +0530

    Remove repeated calls to refresh_all in Windows on settings change. Fixes Issue #15

--- set_window_title.py
+++ set_window_title.py
@@ -41,21 +41,7 @@ def plugin_loaded():
   global _READY_
   _READY_ = True
 
-  # Update all window titles on setting change.
-  settings = sublime.load_settings("set_window_title.sublime-settings")
-  setting_keys = [
-    "unregistered",
-    "template",
-    "has_project_true",
-    "has_project_false",
-    "is_dirty_true",
-    "is_dirty_false",
-    "path_display",
-    "untitled",
-  ]
-  for k in setting_keys:
-    settings.add_on_change(k, refresh_all)
-
+  # Update all window titles on plugin loaded. Plugin will also be reloaded on settings change.
   if PLATFORM == "linux":
     # Set the title when the plugin is loaded.
     # Only enabled on Linux because for some reason it freezes ST on Windows.
@@ -143,7 +129,8 @@ class SetWindowTitle(EventListener):
   def rename_window_windows(self, new_title):
     # PX_WINDOW_CLASS is the ClassName of SublimeText, can be seen via a tool such as Nirsoft WinLister
     hwndSublime = ctypes.windll.user32.FindWindowA(b'PX_WINDOW_CLASS', None)
-    ctypes.windll.user32.SetWindowTextW(hwndSublime, new_title)
+    if(hwndSublime):
+      ctypes.windll.user32.SetWindowTextW(hwndSublime, new_title)
 
 def get_project(window):
   """Returns the project name for the given window.
